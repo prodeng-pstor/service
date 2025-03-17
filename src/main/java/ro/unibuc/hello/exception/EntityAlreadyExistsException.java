d9909cea3caebde30cd2eb90418238a1109aba8f
RaduIoana <115888423+RaduIoana@users.noreply.github.com>
2025-03-17 19:32:09 +0200

    feat: jwt auth and basic crud (#2)
    * feat: add postgres and base jpa entity
    
    * chore: move entities to dedicated folder
    
    * feat!: add basic JWT authentication
    
    * feat: add simple CRUD
    
    * chore: remove commented code
    
    ---------
    
    Co-authored-by: Stefan Zaharie <stefan@zaha.tech>

--- src/main/java/ro/unibuc/hello/exception/EntityAlreadyExistsException.java
+++ src/main/java/ro/unibuc/hello/exception/EntityAlreadyExistsException.java
@@ -1,10 +1,11 @@
 package ro.unibuc.hello.exception;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EntityAlreadyExistsException extends RuntimeException {
+    public EntityAlreadyExistsException(String message) {
         super(message);
     }
 }
